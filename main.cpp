5405e916b2233b6d4efa81175b0f1d9b46b589e2
liberalsy <liberal.sy@gmail.com>
2022-02-08 21:59:46 +0100

    test test

--- main.cpp
+++ main.cpp
@@ -6,5 +6,8 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "Hello, World!" << std::endl;
 
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
